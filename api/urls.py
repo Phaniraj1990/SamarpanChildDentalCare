7cda7bbf77e2b782aa5d4cb486c45e77e68765e0
Phaniraj <badrinarayanpaniraj1990@gmail.com>
2026-04-22 12:51:43 +0530

    fixed url routing

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('contact', views.contact_inquiry, name='contact'),
-    path('clinic-info', views.clinic_info, name='clinic-info'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('clinic-info/', views.clinic_info, name='clinic-info'),
 ]
